6337c5daeb3ca845ca36d2e3d23a424b10de1629
Хван Дмитрий Максимович <drestbm@gmail.com>
2020-06-26 16:07:47 +0700

    update first page

--- src/app/list-of-films/list-of-films.component.ts
+++ src/app/list-of-films/list-of-films.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListOfFilmsService } from '../shared/list-of-films.service'
+import { GenreService } from '../shared/genre.service'
 
 @Component({
   selector: 'app-list-of-films',
@@ -11,13 +12,17 @@ import { ListOfFilmsService } from '../shared/list-of-films.service'
 export class ListOfFilmsComponent implements OnInit {
   private loading: boolean = true
   
-  constructor(public listOfFilmsService:ListOfFilmsService) {}
+  constructor(public listOfFilmsService:ListOfFilmsService, public genresService:GenreService) {}
 
   ngOnInit(): void {
     this.listOfFilmsService.downloadList()
       .subscribe(() => {
         this.loading = false
       })
+    this.genresService.downloadList()
+      .subscribe(() => {
+        this.loading = false
+    })
   }
 
   
--- src/app/shared/genre.service.ts
+++ src/app/shared/genre.service.ts
@@ -13,4 +13,12 @@ export class GenreService {
     downloadList(): Observable<Genres> {
         return this.http.get<Genres>('https://api.themoviedb.org/3/genre/movie/list?api_key=e4035ae71649bd95de62e663316862ab&language=ru-Ru').pipe(tap(list => this.list = list))
     }
+
+    sortGenres() {
+        this.list.genres.sort((a,b)=>{return a.id-b.id});
+    }
+
+    searchGenres(id): string {
+        return this.list.genres.find(item => item.id === id).name;
+    }
 }
